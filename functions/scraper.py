e02e206ac12461f273c26fd20ebd361e0aa33515
sojebsikder <sojebsikder@gmail.com>
2022-03-02 19:43:27 +0600

    main

---
+++ functions/scraper.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup as soup
+
+
+class Scraper:
+
+    # Scrap data from the website
+    def scrap_data(el="div", attr="class", value="col-xs-12 data-row-top"):
+        url = 'https://medex.com.bd/companies/106/incepta-pharmaceuticals-ltd/brands'
+        html = requests.get(url=url)
+
+        if html.status_code == 200:
+            bsobj = soup(html.content, 'lxml')
+
+            product_name = []
+
+            for name in bsobj.findAll(el, {attr: value}):
+                product_name.append(name.text.strip())
+            return product_name
+        else:
+            print(html.status_code)
+            print('Website is not available')
--- scraper.py
+++ scraper.py
@@ -1,24 +1,5 @@
-import requests
-from bs4 import BeautifulSoup as soup
-
-url = 'https://medex.com.bd/companies/106/incepta-pharmaceuticals-ltd/brands'
-
-html = requests.get(url=url)
+from functions.scraper import Scraper
 
 
-if html.status_code == 200:
-    bsobj = soup(html.content, 'lxml')
-
-    product_name = []
-    product_description = []
-    product_price = []
-
-    print("wait...")
-
-    for name in bsobj.findAll('div', {'class': 'col-xs-12 data-row-top'}):
-        product_name.append(name.text.strip())
-    print(product_name)
-    print(product_name.__len__())
-else:
-    print(html.status_code)
-    print('Website is not available')
+data = Scraper.scrap_data()
+print(data)
