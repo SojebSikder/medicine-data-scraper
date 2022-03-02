0fbbfbcd8f98f58f8d40e9c6d7fbe7c89115c988
sojebsikder <sojebsikder@gmail.com>
2022-03-02 20:59:15 +0600

    main

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from functions.scraper import Scraper
 
 url = 'https://medex.com.bd/companies/106/incepta-pharmaceuticals-ltd/brands'
-product_name = Scraper.scrap_data(url=url)
-print(product_name)
+details_url = 'https://medex.com.bd/brands/32000/abdolax-5mg'
+
+names = Scraper.scrap_data(url=url)
+imgs = Scraper.scrap_data(url=details_url)
+contents = Scraper.scrap_data(url=details_url,value='ac-body')
+
+print(contents)
